f302080e3f580a32388ef98f3b7fea8b7e4d0922
bei-ko <140593339+bei-ko@users.noreply.github.com>
2024-08-15 18:49:52 +0800

    Add files via upload

---
+++ thm-main/global.js
@@ -0,0 +1,289 @@
+var touhouMobile = {
+    stg : {
+        "东方夏夜祭" : {
+            url : "http://m.xz7.com/downinfo/596997.html",
+            info : "一款由电脑移植的车万stg游戏<br>从手感和体验来说都相当不错w<br>不太适合新手玩<br>",
+            star : 5
+        },
+        "东方恋觉想" : {
+            url : "https://www.lanzouh.com/i76S725t076f",
+            info : "一个手机独占的围绕地灵殿展开的stg作<br>总的来说很不错<br>手感差一些<br>",
+            star : 4
+        },
+        "东方千夜帖" : {
+            url : "https://m.newasp.com/android/702908.html",
+            info : "一款相当好的车万stg手游<br>收录了大部分正作的角色，符卡，自机<br>推荐新手试一试<br>",
+            star : 5
+        },
+        "东方魔导录" : {
+            url : "https://www.168you.com/game/12507.html",
+            info : "手感很不错，UI也很不错，很流畅<br>支持莎莎，灵梦，早苗，妖梦，咲夜五中自机<br>类似于上者，比上者功能多一些，ui更好看<br>符卡没有上者多<br>",
+            star : 5
+        },
+        "东方幻灵梦" : {
+            url : "http://m.downcc.com/d/56720",
+            info : "这作真的超烂，操作有点小问题不重要<br>对新手来说很难，而且素材也很烂，直接用的黄昏绘<br> ---来自LoveKogasa的吐槽<br>",
+            star : 2
+        },
+        "BakaSTG" : {
+            url : "http://m.downcc.com/d/284948",
+            info : "一个经典风的车万STG手游<br>手感不错，风格也很不错w<br>https://minagi.me/archives/1552<br>",
+            star : 4
+        },
+        "弹幕游戏" : {
+            url : "http://m.downcc.com/d/284934",
+            info : "类似于上者，也是一个经典风的车万STG手游<br>总体来说还是挺不错的<br>",
+            star : 4
+        },
+        "东方有顶天" : {
+            url : "https://www.lanzouh.com/iHtIY260jtkh",
+            info : "一个车万手游STG作，质量还行<br>提供者未提供介绍，我以前也没通关过<br>所以只能说到这些<br>",
+            star : 4
+        },
+        "东方灵寻梦" : {
+            url : "https://www.lanzouh.com/iDHGJ260juoh",
+            info : "这个挺不错的，应该是新版本吧<br>提供者未提供描述，<s>我只玩过有bug的旧版本</s><br>",
+            star : 4
+        },
+        "东方圣光叶" : {
+            url : "https://www.lanzouh.com/iE0w0260juxg",
+            info : "琪露诺自机，莎莎敌机的同人stg<br>好像只有他俩？没有判定点，但是场景很大<br>总体质量一般<br>",
+            star : 3
+        },
+        "爱丽丝の弾幕修行" : {
+            url : "https://www.lanzouh.com/iFzpR260jrte",
+            info : "横板stg，操控爱丽丝和人偶攻打红魔乡的角色<br>",
+            star : 4
+        },
+        "东方幸存者" : {
+            url : "https://www.lanzouh.com/iVO7a260jq0j",
+            info : "结合了子弹地狱元素的弹幕肉鸽游戏<br>",
+            star : 5
+        }
+    },
+    音游 : {
+        "ダンカグライク" : {
+            url : "https://www.lanzouh.com/irq9p25t005c",
+            info : "提供者未添加描述<br>",
+            star : 3
+        },
+        "TouhouMix" : {
+            url : "https://cloud.wujiyan.cc/s/7vDCl",
+            info : "一个相当好的车万音游手游<br>",
+            star : 5
+        },
+        "东方曲包" : {
+            url : "https://cloud.wujiyan.cc/s/ewMfr",
+            info : "与上面两个音游配套食用的东方曲曲包",
+            star : 4
+        },
+        " ファンシーライブ" : {
+            url : "https://www.lanzouh.com/i9c3k260jsof",
+            info : "音游，但目前曲子比较少",
+            star : 5
+        }
+    },
+     
+    回合制 : {
+        "东方异想穴" : {
+            url : "http://m.downcc.com/d/347952",
+            info : "一个挺不错的车万同人回合制游戏<br>后期很爽w<br>",
+            star : 4
+        },
+        "东方地阴风" : {
+            url : "https://www.lanzouh.com/i6CEG25pdm0d",
+            info : "3D风回合制游戏，暂无汉化<br>",
+            star : 3
+        },
+        "灵梦无双" : {
+            url : "https://www.lanzouh.com/iZt83260jwni",
+            info : "考验反应能力，在出招的瞬间破解<br>",
+            star : 5
+        }
+    },
+    地牢 : {
+        "觉觉地牢" : {
+            url : "https://www.lanzouh.com/iirjj25t0a5c",
+            info : "扮演地灵殿的角色的地牢游戏<br>",
+            star : 4
+        },
+        "夜临东方地下城GS1.6" : {
+            url : "https://www.lanzouh.com/iF1Sx25t0exe",
+            info : "地下城RPG游戏<br>",
+            star : 4
+        },
+        "夜临东方地下城1.8.9" : {
+            url : "https://www.lanzouh.com/ibmOk25t0fxac",
+            info : "地下城RPG游戏<br>",
+            star : 4
+        },
+        "厝Chuuo：私は厝" : {
+            url : "https://www.lanzouh.com/i4Yfj25t02kj",
+            info : "地牢类游戏<br>",
+            star : 3
+        },
+         "东方像素地牢" : {
+            url : "https://www.lanzouh.com/iuaH525szxrg",
+            info : "像素地牢是一个非常经典的地牢游戏，这个是东方版的",
+            star : 4
+         }
+    },
+    跑酷 : {
+        "东方梦奔跑" : {
+            url : "https://www.lanzouh.com/iApFp260jsri",
+            info : "提供者暂未提供描述，请等待更新",
+            star : 0
+        }
+    },
+    RPG : {
+        "TTB" : {
+            url : "https://www.lanzouh.com/iVDuy260jqpe",
+            info : "和东方rpg同一个作者同一个类型，立绘不错，但非常肝<br>",
+            star : 3
+        },
+         "东方RPG" : {
+            url : "https://www.lanzouh.com/iKfy7260jube",
+            info : "和TTB同一个作者同一个类型，立绘不错，但非常肝<br>",
+            star : 3
+        },
+        "东方VGS" : {
+            url : "https://www.lanzouh.com/ivPuK260juha",
+            info : "提供者暂未提供描述，请等待更新",
+            star : 0
+        },
+         " レミリアvsパチュリー" : {
+            url : "https://www.lanzouh.com/i9c3k260jsof",
+            info : "闯关RPG，非常可爱，但难度偏高<br>",
+            star : 5
+        }
+    },
+    其他 : {
+        "踢屁股大赛" : {
+            url : "https://www.lanzouh.com/iBU4R25t0daf",
+            info : "蓄力把各种角色踢到尽可能远的地方吧<br><s>好胃炎的馆主www</s><br>",
+            star : 4
+        },
+        "⑨的完美算数教室(游戏)" : {
+            url : "https://www.lanzouh.com/isCIM25t0cuj",
+            info : "BAKA BAKA<br>",
+            star : 5
+        },
+         "雏酱Breaker 2nd.break" : {
+            url : "https://cloud.wujiyan.cc/s/xMzUk",
+            info : "键山雏横板对战手游，暂无汉化<br>",
+            star : 5
+        },
+         "蕾米莉亚的晚餐(汉化)" : {
+            url : "https://www.lanzouh.com/i2PP425t0b0d",
+            info : "和上面的雏酱和踢屁股大赛是同一个作者，非常不错的东方小游戏口牙<br>",
+            star : 5
+        },
+         "东方幻想乡防卫记" : {
+            url : "https://www.lanzouh.com/i0eAu25t04ri",
+            info : "东方塔防游戏<br>",
+            star : 5
+        },
+         "东方FM" : {
+            url : "https://www.lanzouh.com/iQ3Vr25szxuj",
+            info : "可以连接到来自幻想乡的电台，非常不错的小软件<br>",
+            star : 4
+        },
+         "东方消消乐" : {
+            url : "https://www.lanzouh.com/iRYKi25t092d",
+            info : "东方主题的消消乐游戏，连成三个即可消掉<br>",
+            star : 4
+        },
+         "东方融合解密RPG" : {
+            url : "https://www.lanzouh.com/ibWo625t08rc",
+            info : "东方x西瓜，可以在融合中解密的RPG游戏<br>",
+            star : 4
+        },
+        "东方幻想乡防卫记2" : {
+            url : "https://www.lanzouh.com/iuo1l25q7ech",
+            info : "上面那个东方幻想乡防卫记的二代，塔防游戏<br>",
+            star : 5
+        },
+        "蕾米莉亚kicker" : {
+            url : "https://www.lanzouh.com/iHemX2607qif",
+            info : "通过迫害蕾米赚钱赎回红魔馆及成员的游戏<br>",
+            star : 4
+        },
+        "东方猜拳录" : {
+            url : "https://www.lanzouh.com/i28oV2637dbi",
+            info : "根据要求来猜拳，坚持尽可能久的时间吧！<br>",
+            star : 4
+        },
+        "雏酱踢屁股大赛" : {
+            url : "https://www.lanzouh.com/iwSyG260jw4j",
+            info : "踢屁股大赛系列，和蕾米踢屁股大赛一样的作者，踢得越远越好<br>",
+            star : 5
+        },
+         "蕾米莉亚掉落" : {
+            url : "https://www.lanzouh.com/iLqx8260jore",
+            info : "可爱休闲游戏，点击物品让蕾米莉亚掉到床上<br>",
+            star : 5
+        },
+         "合成大翠香" : {
+            url : "https://www.lanzouh.com/iKUut260jpmf",
+            info : "很可爱的休闲小游戏，把各种角色合成翠香<br>",
+            star : 4
+        },
+
+    },
+    
+    暂未分类 : {
+        "琪露诺坦克" : {
+            url : "https://www.lanzouh.com/i2UP7260jofc",
+            info : "提供者暂未提供描述，请等待更新(这个是谷歌游戏，似乎玩不了)",
+            star : 0
+        },
+        /*"Satori 的像素地牢" : {
+            url : "https://www.lanzouh.com/iVZvT260jpib",
+            info : "提供者暂未提供描述，请等待更新",
+            star : 0
+        },这个就是觉地牢*/
+       
+        "东方玉灵宫(破解版)" : {
+            url : "https://www.lanzouh.com/i1UDY260jvod",
+            info : "提供者暂未提供描述，请等待更新",
+            star : 0
+        },
+        "未だ知られぬ物語" : {
+            url : "https://www.lanzouh.com/iVOS0260k5aj",
+            info : "提供者暂未提供描述，请等待更新",
+            star : 0
+        }
+    },
+    非 : {
+        "Mc车万皮肤集合" : {
+            url : "https://www.lanzouh.com/iGlCS25plc4f",
+            info : "东方Project Minecraft皮肤集合<br>",
+            star : 5
+        },
+        "互动壁纸" : {
+            url : "https://www.lanzouh.com/i9RZq25plc9a",
+            info : "车万角色的互动壁纸，挺有意思的w<br>",
+            star : 5
+        },
+        "一些PDF资源" : {
+            url : "https://www.lanzouh.com/iVcz025pld1i",
+            info : "一些Pdf资源，建议新人看看w<br>",
+            star : 5
+        },
+        "东方圣诞节主题头像" : {
+            url : "https://www.lanzouh.com/i3Dcv25pnkub",
+            info : "东方角色圣诞节主题头像，提取码thm",
+            star : 4
+        },
+         "东方电池插件" : {
+            url : "https://www.lanzouh.com/iMg3x260jspg",
+            info : "可以在手机上养东方角色的小部件<br>",
+            star : 5
+        },
+         "Google Play" : {
+            url : "https://www.lanzouh.com/ishnY25szwud",
+            info : "谷歌应用市场，非常重要的前置依赖库，部分游戏需要依赖此程序运行",
+            star : 5
+         }
+    }
+}
